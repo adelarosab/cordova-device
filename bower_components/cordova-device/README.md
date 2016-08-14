b53844f6724dd85633cc09314b4b19ccd18ea636
Adrián de la Rosa <adrian@delarosab.me>
2016-08-15 00:39:12 +0200

    Initial commit gh-pages

---
+++ bower_components/cordova-device/README.md
@@ -0,0 +1,63 @@
+_[Demo and API docs](https://adelarosab.github.io/cordova-device)_
+
+## Note:
+Due to restrictions `ready` attribute is not shown into attributes table.
+
+## Installation
+In your `www` project:
+`bower install --save `
+
+In your `cordova` project:
+`cordova plugin add cordova-plugin-device`
+
+## &lt;cordova-device&gt;
+
+`<cordova-core>` describes the device's hardware and software.
+
+```html
+<cordova-core
+    cordova="6.3.0"
+    manufacturer="Apple"
+    model="Passion"
+    platform="iOS"
+    ready
+    serial="XXX"
+></cordova-core>
+```
+### Attributes
+ 
+#### cordova (read-only)
+
+Return the version of Cordova running on the device.
+ 
+#### manufacturer (read-only)
+
+Return the device's manufacturer.
+ 
+#### model (read-only)
+
+Return the name of the device's model or product. (Nexus One returns "Passion").
+ 
+#### platform (read-only)
+
+Return the device's operating system name. ("iOS").
+ 
+#### ready (read-only)
+
+Return if cordova deviceready event has been fired.
+ 
+#### serial (read-only)
+
+Return the device hardware serial number.
+ 
+#### uuid (read-only)
+
+Return the device's Universally Unique Identifier.
+ 
+#### version (read-only)
+
+Return the operating system version.
+ 
+#### virtual (read-only)
+
+Return whether the device is running on a simulator.
---
+++ bower_components/cordova-device/cordova-device.js
@@ -0,0 +1,112 @@
+'use strict';
+
+Polymer(
+  {
+    is: 'cordova-device',
+
+    properties: {
+      /**
+       * Return the version of Cordova running on the device.
+       */
+      cordova: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return the device's manufacturer.
+       */
+      manufacturer: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return the name of the device's model or product. (Nexus One returns
+       * "Passion").
+       */
+      model: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return the device's operating system name. ("iOS").
+       */
+      platform: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return if cordova deviceready event has been fired.
+       */
+      ready: {
+        computed: '_computeReady(_ready_, _paused_)',
+        notify: true,
+        observer: '_observeReady',
+        type: Boolean,
+        value: false
+      },
+
+      /**
+       * Return the device hardware serial number.
+       */
+      serial: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return the device's Universally Unique Identifier.
+       */
+      uuid: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return the operating system version.
+       */
+      version: {
+        notify: true,
+        readOnly: true,
+        type: String
+      },
+
+      /**
+       * Return whether the device is running on a simulator.
+       */
+      virtual: {
+        notify: true,
+        readOnly: true,
+        type: Boolean
+      }
+    },
+
+    _computeReady(ready, paused) {
+      return ready && !paused;
+    },
+
+    _observeReady(ready) {
+      if (ready) {
+        const device = window.device;
+
+        this._setCordova(device.cordova);
+        this._setManufacturer(device.manufacturer);
+        this._setModel(device.model);
+        this._setPlatform(device.platform);
+        this._setSerial(device.serial);
+        this._setUuid(device.uuid);
+        this._setVersion(device.version);
+        this._setVirtual(device.isVirtual);
+      }
+    }
+  }
+);
